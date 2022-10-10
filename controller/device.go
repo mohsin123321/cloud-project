a640461b31325303d156e016d82a713c2248e9e0
srajabzadeh <sara.rajabzadeh@sorint.it>
2022-10-10 11:56:24 +0200

    added swagger

--- controller/device.go
+++ controller/device.go
@@ -7,6 +7,13 @@ import (
 	"github.com/mohsin123321/cloud-project/utility"
 )
 
+// CreateClassroom insert the coming data from simulator.
+// @Summary create sensors data.
+// @tags device
+// @Accept json
+// @Param Body body dto.PostDataBody true "contains the information related to device"
+// @Success 200
+// @Router /device [post]
 func (ctrl *HttpController) InsertData(w http.ResponseWriter, r *http.Request) {
 	var body dto.PostDataBody
 	ctrl.Ut.ParseBody(r.Body, &body)
